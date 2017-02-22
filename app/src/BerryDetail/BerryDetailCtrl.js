d16b5e445fa0d4395cb5ccfca41a2e32faae011f
slavitto <slavitto@gmail.com>
2017-02-22 08:20:32 +0300

    BerryDetail ok

---
+++ app/src/BerryDetail/BerryDetailCtrl.js
@@ -0,0 +1,23 @@
+'use strict';
+
+pokemonApp.controller('BerryDetailCtrl', function($scope, $routeParams, BerriesService) {
+
+    $scope.berryLoaded = false;
+
+    $scope.berry = BerriesService.get({
+        berryId: $routeParams['berryId']
+    }, function(successResult) {
+        // Окей!
+        console.log(successResult);
+        $scope.notfoundError = false;
+        $scope.berryLoaded = true;
+    }, function(errorResult) {
+        // Не окей..
+        $scope.notfoundError = true;
+        $scope.berryLoaded = true;
+    });
+
+    $scope.berry.$promise.then(function(result) {
+        //$scope.berryLoaded = true;
+    });
+});
